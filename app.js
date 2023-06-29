06a2dba41bcbbf50d772efbbde1e9f98fa68844a
edgarromero1994 <edgarromerocuc94@gmail.com>
2023-06-29 11:27:56 -0600

    backend acutalizado

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
-const bodyParser = require('body-parser')
-const mongoose = require("mongoose")
-const cors = require("cors")
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-app.use(express.json())
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-//conexion a base de datos
+// Conexión a la base de datos
 const user = "edgar94";
-const password ="oLbVSOWQiWOiZcmk";
-const bdname= "estudiante"
+const password = "oLbVSOWQiWOiZcmk";
+const bdname = "estudiante";
 const uri = `mongodb+srv://${user}:${password}@cluster0.cgpunkw.mongodb.net/${bdname}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,}
-).then(() => console.log("base de datos conectados"))
-.catch(error => console.log(error));
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("Base de datos conectada"))
+  .catch(error => console.log("Error en la conexión a la base de datos:", error));
 
-//para compilar la vista 
-app.use(cors())
+// Rutas
+app.use("/", require("./router/Carnets"));
 
-//RUTAS WEB
-app.use("/", require("./router/RutasWeb"));
-app.use("/", require("./router/Carnets"))
+// Ruta de inicio
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
-  
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Error del servidor');
-  });
-
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Error del servidor');
+});
 
 app.listen(port, () => {
-    console.log("servidor a su servicio en el puerto", port)
-})
-
+  console.log("Servidor en funcionamiento en el puerto", port);
+});
 
